afbdee7a3c1cd8d78ef55db7e1a8766bf2f66056
Yogeshwarik <46434622+Yogeshwarik@users.noreply.github.com>
2019-01-08 15:49:58 +0530

    Add files via upload

---
+++ perfect_square.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(i = 0; i <= n; a++)
+    {
+        if (i == a * a)
+        {
+            printf("perfect square");
+            break;
+        }
+    }
+    printf("not perfect square");
+    return 0;
+}
